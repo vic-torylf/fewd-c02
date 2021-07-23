1ba6f69d789c6e6660637090712b5a1192d41226
vic-torylf <victoryipstu@gmail.com>
2021-07-23 14:34:30 +0800

    "race day v2"

--- codecademy_exercise/Learn Javascript/Race_Day/main.js
+++ codecademy_exercise/Learn Javascript/Race_Day/main.js
@@ -3,17 +3,18 @@ let raceNumber = Math.floor(Math.random() * 1000);
 const registeredEarly = true;
 const runnerAge = 19;
 
-if (registeredEarly && runnerAge >= 18) {
+if (registeredEarly && runnerAge > 18) {
   raceNumber += 1000;
 }
 
 if (registeredEarly && runnerAge > 18) {
-  console.log(`your race number is ${raceNumber}, and your race will start at 9:30 am.`);
+  console.log(`your race number is ${raceNumber}, and you will race at 9:30 am.`);
 } else if (runnerAge > 18) {
-  console.log(`Your race number is ${raceNumber} and your race will start at 11:00 am.`);
+  console.log(`Your race number is ${raceNumber} and you will race at 11:00 am.`);
 } else if (runnerAge < 18){
-  console.log(`Youth registrants will run at 12:30 pm and your race number is ${raceNumber}.`);
+  console.log(`Your race number is ${raceNumber} and you will race at 12:30 am.`);
 } else {
   console.log('go see the registration desk');
 }
+
 
