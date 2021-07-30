f7b052fe8e7efcf3aa696798ca0a8ce5950de5c0
vic-torylf <victoryipstu@gmail.com>
2021-07-31 00:50:11 +0800

    "added mini linter"

---
+++ codecademy_exercise/Learn Javascript/Mini_Linter/main.js
@@ -0,0 +1,46 @@
+let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+
+let overusedWords = ['really', 'very', 'basically'];
+
+let unnecessaryWords = ['extremely', 'literally', 'actually' ];
+
+// Task 1
+const storyWords = story.split(' ');
+// Task 2
+console.log(storyWords.length);
+// Task 3
+const betterWords = storyWords.filter( word => {
+  if (unnecessaryWords.includes(word)){
+    return false;
+  } else {
+    return true;
+  }
+})
+// Task 4
+const overusedWordsCount = storyWords.filter( word => {
+  if (overusedWords.includes(word)){
+    return true;
+  } else {
+    return false;
+  }
+})
+// Task 5
+const storyWordsStrict = story.split('');
+const sentenceCount = storyWordsStrict.filter( mark => {
+  if ('.'.includes(mark) || '!'.includes(mark)) {
+    return true;
+  } else {
+    return false;
+  }
+})
+// Task 6
+const logThreeItems = () => {
+  console.log('================================');
+  console.log(`The word count: ${storyWords.length}
+The sentence count: ${overusedWordsCount.length}
+The number of times each overused word appears: ${sentenceCount.length} times`)
+console.log('================================');
+}
+logThreeItems()
+// Task 7
+console.log(betterWords.join(' '))
