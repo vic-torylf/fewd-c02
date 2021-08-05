b29f45c7bdc8bc8fcdb20a401cac464678200fd7
vic-torylf <victoryipstu@gmail.com>
2021-08-05 15:46:01 +0800

    "added School Catalog"

---
+++ codecademy_exercise/Learn Javascript/School_Catalogue/app.js
@@ -0,0 +1,67 @@
+class School {
+    constructor(name, level, numberOfStudents) {
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
+    }
+    get name() {
+        return this._name;
+    }
+    get level() {
+        return this._level;
+    }
+    get numberOfStudents() {
+        return this._numberOfStudents;
+    }
+    quickFacts() {
+        console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`)
+    }
+    static pickSubstituteTeacher(substituteTeachers) {
+        return substituteTeachers[Math.floor(substituteTeachers.length * Math.random())];
+    }
+    set numberOfStudents(newNumberOfStudents) {
+        if (typeof newNumberOfStudents == 'number') {
+            this._numberOfStudents = newNumberOfStudents; 
+        } else console.log('Invalid input: numberOfStudents must be set to a number.');
+    }
+}
+
+// Primary School
+console.log('  Primary School:')
+class PrimarySchool extends School {
+    constructor(name, numberOfStudents, pickupPolicy) {
+        super(name, 'primary' , numberOfStudents);
+        this._pickupPolicy = pickupPolicy;
+    }
+    get pickupPolicy() {
+        return this._pickupPolicy;
+    }
+}
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+lorraineHansbury.quickFacts();
+console.log(School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']));
+
+// High School
+console.log('  High School:')
+class HighSchool extends School {
+    constructor(name, numberOfStudents, sportsTeams) {
+        super(name, 'high', numberOfStudents);
+        this._sportsTeams = sportsTeams;
+    }
+    get sportsTeams() {
+        return this._sportsTeams;
+    }
+}
+
+const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+alSmith.quickFacts();
+console.log(alSmith.sportsTeams);
+
+//Middle School
+class MiddleSchool extends School {
+    constructor(name, numberOfStudents) {
+        super(name, 'middle', numberOfStudents);
+    }
+}
+
