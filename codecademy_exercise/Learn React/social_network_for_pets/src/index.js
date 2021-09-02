1df888639e54ed4e654c8ac89e723f846b7ed82b
vic-torylf <victoryipstu@gmail.com>
2021-09-02 13:25:42 +0800

    " added social network for pets and updated css"

--- codecademy_exercise/Learn React/social_network_for_pets/src/index.js
+++ codecademy_exercise/Learn React/social_network_for_pets/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Contact from './Contact';
+import App from './App';
 import './styles.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Contact />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
